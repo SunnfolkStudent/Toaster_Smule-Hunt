3464c18517860d61441e1c2f5dcd031bf944da55
hatdog42 <kristian.lillesand@gmail.com>
2024-02-09 13:36:00 +0100

    hazard

--- Assets/Scripts/LevelScripts/spawning/SmuleSpawner.cs
+++ Assets/Scripts/LevelScripts/spawning/SmuleSpawner.cs
@@ -4,10 +4,12 @@ using Random = UnityEngine.Random;
 public class SmuleSpawner : MonoBehaviour
 {
     [SerializeField] private float _spawnRate;
+    [SerializeField] private float _spawnRateHazard;
     
     ObjectPooling objectPooling;
     
     private float _timer;
+    private float _timer2;
 
     private void Start()
     {
@@ -16,10 +18,8 @@ public class SmuleSpawner : MonoBehaviour
     
     private void Update()
     {
-        _timer += Time.deltaTime;
-        if (!(_timer >= _spawnRate)) return;
-        _timer = 0;
-        objectPooling.SpawnFormPool("smule", FindRandomPosition(), Quaternion.identity);
+        SpawnSmule();
+        SpawnHazard();
     }
 
     private Vector3 FindRandomPosition()
@@ -27,5 +27,20 @@ public class SmuleSpawner : MonoBehaviour
         var randomPos = new Vector3(Random.Range(-9, 9), Random.Range(-5, 5), 0);
         return randomPos;
     }
-    
+
+    private void SpawnSmule()
+    {
+        _timer += Time.deltaTime;
+        if (!(_timer >= _spawnRate)) return;
+        _timer = 0;
+        objectPooling.SpawnFormPool("smule", FindRandomPosition(), Quaternion.identity);
+    }
+
+    private void SpawnHazard()
+    {
+        _timer2 += Time.deltaTime;
+        if (!(_timer2 >= _spawnRateHazard)) return;
+        _timer2 = 0;
+        objectPooling.SpawnFormPool("hazard", FindRandomPosition(), Quaternion.identity);
+    }
 }
--- Assets/Scripts/scoreScripts/score.cs
+++ Assets/Scripts/scoreScripts/score.cs
@@ -4,6 +4,9 @@ using UnityEngine.UI;
 public class score : MonoBehaviour
 {
     public Text text;
-    
-    
+
+    private void Update()
+    {
+        text.text = scoreingAndDeactivating.score.ToString();
+    }
 }
